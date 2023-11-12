46380ff32b151041e9518b0d8d65cd4596c6a3ce
dudakovict <dudakovict@gmail.com>
2023-11-12 23:57:12 +0100

    added grpc gateway

---
+++ gapi/rpc_login_user.go
@@ -0,0 +1,81 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+
+	db "github.com/dudakovict/simplebank/db/sqlc"
+	"github.com/dudakovict/simplebank/pb"
+	"github.com/dudakovict/simplebank/util"
+	"github.com/dudakovict/simplebank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (s *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
+	user, err := s.store.GetUser(ctx, req.GetUsername())
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user not found")
+		}
+		return nil, status.Errorf(codes.Internal, "failed to find user: %s", err)
+	}
+
+	if err := util.CheckPassword(req.GetPassword(), user.HashedPassword); err != nil {
+		return nil, status.Errorf(codes.NotFound, "incorrect password: %s", err)
+	}
+
+	accessToken, accessPayload, err := s.tokenMaker.CreateToken(user.Username, s.config.AccessTokenDuration)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create  access token: %s", err)
+	}
+
+	refreshToken, refreshPayload, err := s.tokenMaker.CreateToken(user.Username, s.config.RefreshTokenDuration)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create refresh token: %s", err)
+	}
+
+	mtdt := s.extractMetadata(ctx)
+	session, err := s.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           refreshPayload.ID,
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIP,
+		IsBlocked:    false,
+		ExpiresAt:    refreshPayload.ExpiresAt,
+	})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create session: %s", err)
+	}
+
+	rsp := pb.LoginUserResponse{
+		SessionId:             session.ID.String(),
+		AccessToken:           accessToken,
+		AccessTokenExpiresAt:  timestamppb.New(accessPayload.ExpiresAt),
+		RefreshToken:          refreshToken,
+		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiresAt),
+		User:                  converUser(user),
+	}
+
+	return &rsp, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	return violations
+}
